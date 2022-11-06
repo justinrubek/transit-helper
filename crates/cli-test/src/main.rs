0af06bc0808eafb938e0c1f7088cd78fa6903bc5
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2022-11-06 14:44:19 -0600

    feat: fetch data directly from source

--- crates/cli-test/src/main.rs
+++ crates/cli-test/src/main.rs
@@ -1,43 +1,56 @@
+use gtfs_rt::{FeedEntity, Position};
 use prost::Message;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let fname = std::env::args().nth(1).expect("missing file name");
-    let data = std::fs::read(fname)?;
-    let message = gtfs_rt::FeedMessage::decode(&*data)?;
+#[derive(Debug)]
+struct VehicleData {
+    route: String,
+    position: Position,
+}
+
+fn get_dart_route(feed: FeedEntity) -> Option<VehicleData> {
+    let vehicle = if let Some(vehicle) = feed.vehicle {
+        vehicle
+    } else {
+        return None;
+    };
+
+    let vehicle_trip = if let Some(trip) = vehicle.trip {
+        trip
+    } else {
+        return None;
+    };
+
+    let route_id = if let Some(route_id) = vehicle_trip.route_id {
+        route_id
+    } else {
+        return None;
+    };
+
+    Some(VehicleData {
+        route: route_id,
+        position: vehicle.position.unwrap(),
+    })
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+
+    let position_resp = client.get("https://www.ridedart.com/gtfs/real-time/vehicle-positions").send().await?;
+
+    let data = position_resp.bytes().await?;
+    let message = gtfs_rt::FeedMessage::decode(data)?;
     // println!("{:?}", message);
 
     // print message.header info
     println!("timestamp: {:?}", message.header.timestamp);
 
     println!("entity count: {}", message.entity.len());
-    for entity in message.entity {
-        // Extract the route_id from entity.vehicle.trip.route_id
-        // where each field is an Option
-        let vehicle = if let Some(vehicle) = entity.vehicle {
-            vehicle
-        } else {
-            continue;
-        };
-
-        let vehicle_trip = if let Some(trip) = vehicle.trip {
-            trip
-        } else {
-            continue;
-        };
-
-        let route_id = if let Some(route_id) = vehicle_trip.route_id {
-            route_id
-        } else {
-            continue;
-        };
-
-        println!(r#"route {:?}
-            position: {:?}
-        "#,
-        route_id,
-        vehicle.position,
-        );
-    }
+    message.entity.iter().for_each(|entity| {
+        if let Some(route_data) = get_dart_route(entity.clone()) {
+            println!("route {} at {:?}", route_data.route, route_data.position);
+        }
+    });
 
     Ok(())
 }
